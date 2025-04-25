03c3dc327ab69fb44b385b806f4f2db2a2188587
chen <chenczn3528@163.com>
2025-04-25 11:06:00 +0800

    reset

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,29 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+//
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/deepspace/',
+// })
+
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/deepspace/',
+  define: {
+    'process.env': {},
+  },
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',  // 配置为 crypto-browserify
+    },
+  },
 })
-
-
