a517ab63eef837029126c10768c818dce1942e63
chen <chenczn3528@163.com>
2025-05-20 00:23:47 +0800

    domain

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,33 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-//
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/deepspace/',
-// })
-
-
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/deepspace/',
+  plugins: [
+      react(),
+    VitePWA({
+      filename: 'sw-beyond.js', // ✅ 与模拟器 A 区分
+      scope: '/deepspace/',   // ✅ base 路径一致
+      registerType: 'autoUpdate',
+      manifest: {
+        name: '恋与深空 抽卡模拟器',
+        short_name: 'deepspace',
+        start_url: '/deepspace/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: 'images/icon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      cleanupOutdatedCaches: true,
+    })],
+  base: '/',
   define: {
     'process.env': {},
   },
