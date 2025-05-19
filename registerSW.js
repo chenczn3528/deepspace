0fd7cc67976772dac0e66aaffe6da1a057c279a4
chenczn3528 <100138908+chenczn3528@users.noreply.github.com>
2025-05-19 23:52:37 +0000

    Deploying to gh-pages from @ chenczn3528/deepspace@1bc8c2e3f39edeb4451d23f49948291ca1be4bff 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw-beyond.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ds-beyond.js', { scope: '/' })})}
